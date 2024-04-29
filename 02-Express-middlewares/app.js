4e76a91d0dff1b08ff3c80b11c985f1a1d5dd01b
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-04-29 11:46:49 +0200

    Exercice de création de middlewares.

--- 02-Express-middlewares/app.js
+++ 02-Express-middlewares/app.js
@@ -1,7 +1,19 @@
 const express = require("express")
 const app = express()
 
-// CODE...
+app.use((req, res, next) => {
+    console.log('First middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('Second middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(201).json({ message: "Le middleware fonctionne !" })
+})
 
 
 module.exports = app
